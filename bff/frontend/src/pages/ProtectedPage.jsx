fe0cad17c71e7b6aa62fd4f92df97432194aeaae
Kim Maida <1884525+kmaida@users.noreply.github.com>
2025-06-30 17:30:37 -0400

    Style the protected page

--- bff/frontend/src/pages/ProtectedPage.jsx
+++ bff/frontend/src/pages/ProtectedPage.jsx
@@ -20,6 +20,7 @@ function ProtectedPage() {
   return (
     <div>
       <h2>Protected Page</h2>
+      <p>This page makes a secured <code>GET</code> request to the backend API to fetch and display a simple message. The user must be logged in and have a valid access token in an <code>HttpOnly</code> cookie in order to retrieve protected API data. The returned data is a simple JSON object with a <code>message</code> property, but you can return your own secure data instead in <code>/backend/src/api.ts</code>.</p>
       {error && <div style={{color: 'red'}}>Error: {error.message}</div>}
       {data ? (
         <pre>{JSON.stringify(data.message, null, 2)}</pre>
