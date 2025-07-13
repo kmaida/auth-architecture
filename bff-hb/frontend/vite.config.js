be420be93ae1c1be85c5e5c66a636eda530fb161
Kim Maida <yi.mihi@gmail.com>
2025-07-13 13:15:26 -0400

    Started restoring resourceapi page to test with proxy

--- bff-hb/frontend/vite.config.js
+++ bff-hb/frontend/vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://resource-api.local:5001',
+        changeOrigin: true,
+        cookieDomainRewrite: '',
+        secure: false
+      }
+    }
   }
 })
